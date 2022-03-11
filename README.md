b85ab102a168ea1f99d9166d62934adcd758a2d4
Nick Babcock <nbabcock19@hotmail.com>
2022-03-11 06:58:36 -0600

    Add jomini

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cloud Functions
+
+A grab bag list of Cloudflare worker functions
---
+++ jomini/src/index.mjs
@@ -0,0 +1,19 @@
+import { Jomini } from "jomini/slim";
+import wasm from "jomini/jomini.wasm";
+
+async function fetch(req) {
+  const jomini = await Jomini.initialize({ wasm });
+
+  const data = new Uint8Array(await req.arrayBuffer());
+  const out = jomini.parseText(data, {}, (q) => q.json());
+  return new Response(out, {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+export default {
+  fetch,
+};
